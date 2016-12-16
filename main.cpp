6391ded8354757c479054eedeffa26f39a5512c1
Elliott Sobek <elliottsobek@gmail.com>
2016-12-15 21:55:08 -0700

    Add verification for coreect file extensions

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <exception>
 #include <cstdlib>
+#include <regex>
 
 using namespace std;
 
@@ -51,13 +52,24 @@ string try_to_open_file(string filename) {
 	}
 }
 
+bool is_correct_extension(string filename) {
+	// Any filename with a .txt or .csv file extension
+	regex target_extension("[0-9A-Za-z]+\\.?(txt|csv)");
+	return regex_match(filename, target_extension);
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 2) {
 		cout << "Usage: ./PCP <filename>" << endl;
 		exit(EXIT_FAILURE);
 	}
+	if (!is_correct_extension(argv[1])) {
+		cout << "Unable to parse file" << endl;
+		exit(EXIT_FAILURE);
+	}
 	string contents = try_to_open_file(argv[1]);
 	cout << "This is contents: " << contents;
+	// CREATE MENU
 	cout << "Done" << endl;
 	return 0;
 }
