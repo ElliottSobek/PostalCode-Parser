2af0e6ccd392fbbfb33921f6c41d4d37c161c2a5
Elliott Sobek <elliottsobek@gmail.com>
2016-12-16 19:40:06 -0700

    Add license header & initial external functions

---
+++ menu.hpp
@@ -0,0 +1,21 @@
+/*
+ * PostalCode Parser
+ * Copyright (C) 2016  Author: Elliott Sobek
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef MENU_HPP
+#define MENU_HPP
+
+void main_menu(void);
+
+#endif /* End MENU_HPP */
