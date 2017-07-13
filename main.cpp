4f158a6b4b3014eb963da92e72c57233ba2df49f
Elliott Sobek <elliottsobek@gmail.com>
2017-07-13 14:21:47 -0600

    Remove template from license

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -57,5 +57,5 @@ int main(int argc, char *argv[]) {
 	}
 	string contents = try_to_open_file(argv[1]);
 	main_menu();
-	return 0;
+	return 1;
 }
